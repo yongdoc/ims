7110fd03cd94b227b0ef62165cf8426dad315884
Yong <ayahyaa17@gmail.com>
2026-06-22 23:07:28 +0700

    update home page

--- ims-front/src/components/home/Hero.tsx
+++ ims-front/src/components/home/Hero.tsx
@@ -2,9 +2,15 @@
 
 export default function Hero() {
     return (
-        <section id="home">
-            <div>
-                Home Section
+        <section id="home" className="relative py-20 md:py-32 bg-gradient-to-br from-sky-50 to-white dark:from-slate-950 dark:to-slate-900">
+            <div className="space-y-8">
+                <div className="space-y-6 text-center">
+                    <div>
+                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-slate-900 dark:text-white">
+                            Inventory Management System
+                        </h1>
+                    </div>
+                </div>
             </div>
         </section>
     )
